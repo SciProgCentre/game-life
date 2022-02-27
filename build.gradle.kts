8dbe942bf76fa445ad5c9d67cf2c062259899589
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2022-02-27 16:39:51 +0300

    Init kotlin gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "space.kscience.simba"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
