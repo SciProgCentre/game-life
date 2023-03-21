0b27851025c8bcf3edcc62c618a67e8d422d045f
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2023-03-21 14:42:47 +0100

    Add another set of akka's configuration files for tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -71,7 +71,7 @@ kotlin {
 
                 implementation("org.slf4j:slf4j-api:1.7.36")
                 implementation("org.slf4j:slf4j-simple:1.7.36")
-                implementation("ch.qos.logback:logback-classic:1.4.4")
+                implementation("ch.qos.logback:logback-classic:1.4.4") // needs JDK 11+
 
                 implementation("io.ktor:ktor-serialization:$ktorVersion")
 //                implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
